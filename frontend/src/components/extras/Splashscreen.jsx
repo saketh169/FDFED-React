81bb45ddf9f6bd25165f12fb96e215f32064b5c3
saketh169 <sakethrupa123@gmail.com>
2025-12-18 18:03:09 +0530

    updated

--- frontend/src/components/extras/Splashscreen.jsx
+++ frontend/src/components/extras/Splashscreen.jsx
@@ -4,7 +4,6 @@ import styles from '../../styles/Splashscreen.module.css'
 const SplashScreen = () => {
   const [loadingText, setLoadingText] = useState('Loading...')
 
-  
   useEffect(() => {
     const loadingMessages = [
       'Loading...',
